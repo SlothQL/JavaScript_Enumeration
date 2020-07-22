8d0d8fff14780e1f505d51c26d664d9fbf329616
Alina <lindenalina.al@gmail.com>
2020-07-22 17:56:52 +0100

    anagram passed

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,14 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    const sortedWord = this.word.toLowerCase().split('').sort().join('');
+    return otherWords.filter(word => {
+        if (word != this.word) {
+        return word.toLowerCase().split('').sort().join('') === sortedWord
+        }
+    });
 }
 
 module.exports = AnagramFinder;
