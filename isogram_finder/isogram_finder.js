dcc18bdcd03e26254d4288b1a08c2b6c83c148f5
Alina <lindenalina.al@gmail.com>
2020-07-22 16:58:31 +0100

    isogram passed using a for loop

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,21 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    // make word case insensitive
+    const lowerCased = this.word.toLowerCase()
+    // turn word into an array of letters
+    const arrayOfLetters = lowerCased.split('');
+    // loop over letters and check if a letter is equal to the next letter in line
+    for (let i=0; i < arrayOfLetters.length -1; i++) {
+        for (let j = i + 1; j < arrayOfLetters.length; j++) {
+            if (arrayOfLetters[i] === arrayOfLetters[j]) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 module.exports = IsogramFinder;
